8297484aee110b7c91a40b5fe18e93dde159a6a9
rvkvit <rvk.vit@gmail.com>
2014-08-21 02:09:11 -0400

    my first change

---
+++ Proj_BPY/componentgroups/CommonGroups.java
@@ -0,0 +1,23 @@
+package componentgroups;
+
+import supportlibraries.*;
+//import businesscomponents.*;
+
+// my first change
+/**
+ * Common Groups class
+ * @author Cognizant
+ */
+public class CommonGroups extends ReusableLibrary
+{
+	/**
+	 * Constructor to initialize the component library
+	 * @param scriptHelper The {@link ScriptHelper} object passed from the {@link DriverScript}
+	 */
+	public CommonGroups(ScriptHelper scriptHelper)
+	{
+		super(scriptHelper);
+	}
+	
+	
+}
